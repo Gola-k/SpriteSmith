b81dde2d40a816ebea9b71e86ef3c8ff6d136358
Gola-k <kunalgola9930@gmail.com>
2024-07-15 22:28:59 +0530

    start

---
+++ newIDE/app/src/GameContext/constants.js
@@ -0,0 +1,4 @@
+import GameMarketplace from './GameMarketplace.json';
+
+export const GameMarketAddress = '0xc9f6CE5711508d1D29AF139F374fEDF6365Bc520';
+export const GameMarketAddressABI = GameMarketplace.abi;
---
+++ newIDE/app/src/context/constants.js
@@ -0,0 +1,7 @@
+import NFTMarketplace from './NFTMarketplace.json';
+
+export const MarketAddress = '0x0ae6352e1C411aE52B64e9F702244eC9bF6e44Ac';
+export const MarketAddressABI = NFTMarketplace.abi;
+
+
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3 localhost address 
